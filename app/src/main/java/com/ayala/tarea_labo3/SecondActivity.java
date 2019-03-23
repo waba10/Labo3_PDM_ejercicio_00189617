f893556c28d31db7c880c0892506b6f3daf9aae1
waba10 <00189617@uca.edu.sv>
2019-03-22 22:04:13 -0600

    Fin

--- app/src/main/java/com/ayala/tarea_labo3/SecondActivity.java
+++ app/src/main/java/com/ayala/tarea_labo3/SecondActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.ayala.tarea_labo3.utils.AppConstant;
+
 public class SecondActivity extends AppCompatActivity {
 
     private TextView mTextUser, mTextPass, mTextEmail, mTextGender;
@@ -30,10 +32,17 @@ public class SecondActivity extends AppCompatActivity {
 
         mButtonShare.setOnClickListener(v->{
             String text=mTextUser.getText().toString();
+            String text1=mTextPass.getText().toString();
+            String text2=mTextEmail.getText().toString();
+            String text3=mTextGender.getText().toString();
+            String text4= text+"\n" + text1+ "\n "+ text2+"\n "+text3;
+
             Intent mIntent2=new Intent();
             mIntent2.setType("text/plain");
             mIntent2.setAction(Intent.ACTION_SEND);
-            mIntent2.putExtra(Intent.EXTRA_TEXT, text);
+            mIntent2.putExtra(Intent.EXTRA_TEXT, text4);
+
+
             startActivity(mIntent2);
         });
     }
--- app/src/main/java/com/ayala/tarea_labo3/utils/AppConstant.java
+++ app/src/main/java/com/ayala/tarea_labo3/utils/AppConstant.java
@@ -1,5 +1,10 @@
 package com.ayala.tarea_labo3.utils;
 
 public class AppConstant {
-    public static String TEXT_KEY="textKey";
+
+    public static final String TEXT_USER="username";
+    public static final String TEXT_PASS="password";
+    public static final String TEXT_EMAIL="email";
+    public static final String TEXT_GENDER="genero";
+
 }
